348edd0d83280dc7ecb441f79832822efc80bf7d
RuDr8A <rudrasingh240310@gmail.com>
2026-07-30 23:56:43 +0530

    created the post and get apis

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express') 
 const postModel = require('./models/post.model')
 const multer = require('multer')
+const uploadFile = require('./services/storage.service')
+
+
 
 
 
@@ -11,10 +14,26 @@ app.use(express.json())
 const upload = multer({storage : multer.memoryStorage()})
 
 app.post('/create-post', upload.single('image'), async (req, res) =>{
-    console.log(req.body)
-    console.log(req.file);
-    res.status(200).json({ message: "File uploaded successfully!" });
     
+    const imageKitResult = await uploadFile(req.file.buffer)
+
+    const newPost = await postModel.create({
+        image : imageKitResult.url,
+        caption : req.body.caption
+    })
+
+    res.status(201).json({ 
+            message: "Post created successfully!",
+            post: newPost
+    });
+    
+})
+app.get('/posts', async (req, res) =>{
+    const posts = await postModel.find() ;
+    res.status(200).json({
+        message : "Fetched successfully",
+        posts : posts
+    })
 })
 
 module.exports = app 
---
+++ src/services/storage.service.js
@@ -0,0 +1,19 @@
+const ImageKit = require("@imagekit/nodejs");
+require("dotenv").config(); 
+
+const imageKit = new ImageKit({
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
+});
+
+async function uploadFile(buffer) {
+  const result = await imageKit.files.upload({
+    file: buffer.toString('base64'),
+    fileName: 'image.png' 
+  });
+  
+  return result;
+}
+
+module.exports = uploadFile;
